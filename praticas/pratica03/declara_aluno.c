4141efe6e0d9f7c9aa917dc5faff573c8d2d90bb
antyverse <sidinho102030@gmail.com>
2024-08-28 19:25:06 +0000

    save

--- praticas/pratica03/declara_aluno.c
+++ praticas/pratica03/declara_aluno.c
@@ -2,16 +2,16 @@
 
 int main() { 
 
-  int matricula = 0;
+  int matricula = 2422082032;
   int idade = 99; 
   float altura = 9.90f;
   float peso = 999.0f;
     char sexo = 'M';
 
-  printf("Matricula: %08i\n", matricula);
+  printf("Matricula: %li\n", matricula); // 'li' significa long int, usado para inteiro longos
   printf("Idade: %02i\n", idade);
-  printf("Altura: %1.2f\n", altura);
-  printf("Peso: %3.1f\n", peso);
+  printf("Altura: %1.2f m\n", altura);
+  printf("Peso: %3.1f kg\n", peso);
   printf("Sexo: %c\n", sexo);
 
 return 0; 
