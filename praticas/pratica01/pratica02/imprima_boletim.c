7fb7e854f5aa3ec1ec9540111d37207af71a461d
antyverse <sidinho102030@gmail.com>
2024-08-29 11:49:57 +0000

    save

--- praticas/pratica01/pratica02/imprima_boletim.c
+++ praticas/pratica01/pratica02/imprima_boletim.c
@@ -1,12 +1,18 @@
 #include <stdio.h>
 
 int main()  { 
+  
+  
+   
+  printf("%s\t\t%s\t%s\t%s\n", "Matricula", "A1", "A2", "Media");
+    
+  printf("%s\t\t%s\t%s\t%s\n", "20210010", "8.5", "5.0", "6.4");
+
+  printf("%s\t\t%s\t%s\t%s\n", "20215053", "4.7", "10.0", "7.9");
+  
+  printf("%s\t\t%s\t%s\t%s\n", "20220027", "5.6", "8.3", "7.2");
+    
+  printf("%s\t\t%s\t%s\t%s\n","20220103", "9.2", "1.5", "4.6");
+  
   return 0;
-  printf()
-    \t 
-    Matricula  A1   A2   Media
-    20210010   8.5  5.0   6.4 
-    20215053   4.7  10.0  7.9
-    20220027   5.6  8.3   7.2
-    20220103   9.2  1.5   4.6
 }
